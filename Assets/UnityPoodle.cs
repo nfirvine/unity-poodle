da478838e08b11aa8cffe4ef13b84c8ff4266170
Nick Irvine <nirvine@bns-g.com>
2015-09-25 15:40:20 -0700

    Working.

---
+++ Assets/UnityPoodle.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Security.Cryptography.X509Certificates;
+using System.Security;
+using System.Net.Security;
+using System.Net;
+using Mono.Security;
+
+public class UnityPoodle : MonoBehaviour {
+	
+	public void Start () {
+		Debug.Log ("Connecting without mitigation");
+		bool unpwned = MyTest (false);
+		if (unpwned) {
+			Debug.Log ("Everything went fine.");
+		} else {
+			Debug.LogError ("You got pwned.");
+		}
+		Debug.Log ("Connection with mitigation");
+		unpwned = MyTest (true);
+		if (unpwned) {
+			Debug.Log ("Everything went fine.");
+		} else {
+			Debug.LogError ("You got pwned.");
+		}
+	}
+	
+	public bool MyTest (bool disableSsl3) {
+		String url = "https://www.ssllabs.com:10300/";
+		bool unpwned = false;
+		SecurityProtocolType oldprot = System.Net.ServicePointManager.SecurityProtocol;
+		if (disableSsl3) {
+			//This TLS is actually TLS1.0
+			System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+		}
+		HttpWebRequest req = WebRequest.Create(url) as HttpWebRequest;
+		HttpWebResponse resp;
+		try {
+			//This will throw an exception with SSL3 disabled, since we can't find an appropriate TLS version
+			resp = req.GetResponse() as HttpWebResponse;
+		} catch (WebException ex) {
+			if (disableSsl3) {
+				unpwned = true;
+			}
+		} 
+		if (disableSsl3) {
+			System.Net.ServicePointManager.SecurityProtocol = oldprot;
+		}
+		return unpwned;
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+unity-poodle
+============
+
+Demonstrates that Unity's System.Net.HttpWebRequest is vulnerable to POODLE in its default configuration.
+
+Mitigation is to disable SSLv3 in lieu of enabling TLS_FALLBACK_SCSV (not supported). That leaves TLS1.0, which is more secure, at least as secure as we'll get until TLS1.1+ are supported. 
